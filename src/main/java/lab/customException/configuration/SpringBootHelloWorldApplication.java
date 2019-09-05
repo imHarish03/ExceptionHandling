5a80987c35c5d015f499a9ff05b0f15e049c96c5
imHarish03 <harishhari302@gmail.com>
2019-09-05 16:43:57 +0530

    Initial Push

---
+++ src/main/java/lab/customException/configuration/SpringBootHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package lab.customException.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("lab.customException")
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+}
---
+++ src/main/java/lab/customException/service/EmployeeService.java
@@ -0,0 +1,30 @@
+package lab.customException.service;
+
+import org.springframework.stereotype.Service;
+
+import lab.customException.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	public Employee getEmployee() throws EmployeeServiceException {
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+
+	public Employee getEmployeeNull() throws EmployeeServiceException {
+
+		return null;
+	}
+
+	public Employee getEmployeeException() throws EmployeeServiceException {
+
+		throw new EmployeeServiceException();
+	}
+
+}
